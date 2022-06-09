5e8ec9b6c12c15f8c0aa567ddaa22e6eae4d05a5
incordios <69522766+incordios@users.noreply.github.com>
2022-06-09 09:40:15 -0700

    setup react functionality

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,12 +2,18 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />;
+      <Route path="/login" element={<Login />} />
+    <Route path="/notes/new" element={<NewNote />} />
+        <Route path="/notes/:id" element={<Notes />} />
+            <Route path="*" element={<NotFound />} />;
     </Routes>
   );
 }
